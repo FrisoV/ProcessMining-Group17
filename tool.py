6727b7422453cf61eefab58d06c190e58688aa66
L.Zolyomi <zolyomilevi@gmail.com>
2021-02-05 17:59:13 +0100

    tool.py

--- tool.py
+++ tool.py
@@ -7,9 +7,9 @@ window.geometry("600x300")
 lbl = Label(window, text = "Input your training and test .csv files, and give a name to the output file:", font=("Helvetica Neue", 12))
 lbl.grid(column=0, row=0)
 
-training_path = filedialog.askopenfilename(filetypes = ("CSV files", "*.csv"))
+training_path = filedialog.askopenfilename(
 
-test_path = filedialog.askopenfilename(filetypes = ("CSV files", "*.csv"))
+test_path = filedialog.askopenfilename()
 
 
 window.mainloop()
