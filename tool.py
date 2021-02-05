4a06ac7774ddba26bd9d0d2351640d463795bf5f
L.Zolyomi <zolyomilevi@gmail.com>
2021-02-05 18:01:51 +0100

    gitlab test

--- tool.py
+++ tool.py
@@ -7,7 +7,7 @@ window.geometry("600x300")
 lbl = Label(window, text = "Input your training and test .csv files, and give a name to the output file:", font=("Helvetica Neue", 12))
 lbl.grid(column=0, row=0)
 
-training_path = filedialog.askopenfilename(
+training_path = filedialog.askopenfilename()
 
 test_path = filedialog.askopenfilename()
 
