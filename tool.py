777f8e9e18f9ade603671e5ecd6bcafa54fb4f7a
L.Zolyomi <zolyomilevi@gmail.com>
2021-02-06 12:28:46 +0100

    made a (somewhat) functional gui

--- tool.py
+++ tool.py
@@ -1,6 +1,15 @@
 from tkinter import filedialog
 from tkinter import *
 
+from most_frequent_opt import baseline_next_event
+
+def print_file():
+    
+    res = "Input files are TRAIN: {}, TEST: {}, Output filename: {}".format(train_path.get(), test_path.get(), output_filename.get())
+    lbl3.configure(text=res)
+    baseline_next_event(train_path.get(), test_path.get(), output_filename.get())
+    lbl3.configure(text="Task done!")
+    
 window = Tk()
 window.title("Process Mining tool")
 window.geometry("600x300")
@@ -9,13 +18,17 @@ lbl.grid(column=0, row=0)
 lbl2 = Label(window, text = "Important! The files should be in the same directory as this file, otherwise specify complete path", font=("Helvetica Neue", 9))
 lbl2.grid(column=0, row=1)
 
-txt = Entry(window, width = 30).grid(column=0, row=2)
-txt2 = Entry(window, width=30).grid(column=0, row=3)
-def print_file():
-    filenames = "Path to training: {} /// Path to test: {}".format(txt.get(), txt2.get())
-    lbl3.configure(text=filenames)
-submit_btn = Button(window, text="Submit", command=print_file).grid(column=0, row=4)
-lbl3 = Label(window, text="Submit the two csv files").grid(column=0, row=5)
+train_path = Entry(window, width = 30)
+train_path.grid(column=0, row=2)
+test_path = Entry(window, width=30)
+test_path.grid(column=0, row=3)
+output_filename = Entry(window, width=30)
+output_filename.grid(column=0, row=4)
+
+submit_btn = Button(window, text="Submit", command=print_file).grid(column=0, row=5)
+lbl3 = Label(window, text="Submit the two csv files")
+lbl3.grid(column=0, row=6)
+
 
 
 window.mainloop()
